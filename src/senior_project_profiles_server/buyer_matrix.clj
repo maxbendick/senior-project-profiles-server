7fe6660078e9d784e8e356ba4586d47f378f3a4a
Max Bendick <maxbendick@gmail.com>
2017-05-10 17:06:34 -0700

    remove interactive code

--- src/senior_project_profiles_server/buyer_matrix.clj
+++ src/senior_project_profiles_server/buyer_matrix.clj
@@ -9,16 +9,10 @@
     (doall
       (csv/read-csv in-file))))
 
-parsed-csv
-
-
 
 (def buyer-matrix
   (transpose (map (fn [row] (map read-string row))
                   (rest (map rest parsed-csv)))))
-
-buyer-matrix
-
 
 
 (def buyer-props
@@ -26,17 +20,11 @@ buyer-matrix
   These correspond to columns in the matrix and elements in the buyer props"
   (into [] (map first (rest parsed-csv))))
 
-buyer-props
-
-
 
 (def buyer-types
   "The first row without the first element.
   These correspond to rows in the matrix."
   (into [] (rest (first parsed-csv))))
-
-buyer-types
-
 
 
 (def buyer {"Openness"          1000
@@ -50,25 +38,27 @@ buyer-types
   [buyer]
   (map (fn [prop] (get buyer prop)) buyer-props))
 
+
 (defn index-to-buyer-type
   [x]
   (get buyer-types x))
+
 
 (defn dominant-index
   [matrix vector]
   (max-index (flatten (mmul matrix (vec-to-col vector)))))
 
+
 (defn buyer-to-buyer-type
   [buyer]
   (index-to-buyer-type (dominant-index buyer-matrix (buyer-to-vec buyer))))
+
 
 (defn vec-to-col
   [x]
   (map (fn [xi] [xi]) x))
 
+
 (defn max-index
   [v]
   (first (apply max-key second (map-indexed vector v))))
-
-
-(buyer-to-buyer-type buyer)

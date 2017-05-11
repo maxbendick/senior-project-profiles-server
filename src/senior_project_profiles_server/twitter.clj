b9651e5f9c59d1d46a35d4915b1325b168349edd
Logan Williams <loganwilliams116@gmail.com>
2017-05-10 19:12:15 -0700

    Added Watson integration and finished Twitter

--- src/senior_project_profiles_server/twitter.clj
+++ src/senior_project_profiles_server/twitter.clj
@@ -2,17 +2,26 @@
   (:use [twitter.oauth]
         [twitter.callbacks]
         [twitter.callbacks.handlers]
-        [twitter.api.restful])
+        [twitter.api.restful]
+        [clojure.string])
   (:import [twitter.callbacks.protocols SyncSingleCallback])
   (:require [clj-http.client :as client]))
 
-(def my-creds (make-oauth-creds *app-consumer-key*
-                                *app-consumer-secret*
-                                *user-access-token*
-                                *user-access-token-secret*))
-
 ; https://github.com/adamwynne/twitter-api
 
+(def CONSUMER_KEY "qUY8nhHgn1ZadBKiIlge36fDR")
+(def CONSUMER_SECRET "sFiCzyAkP8VvQgRwBt9Qh8saj2IlKldTi103SgJRPSvDe8y01R")
+(def ACCESS_TOKEN "830950575189610496-Ha2m186OtAs25KXZpAHpEoeXrkt4h5L")
+(def ACCESS_TOKEN_SECRET "gipc6GI2jjGUEMKNNypqwBpCvvGuged4ULVFusnepUaBH")
+(def creds (make-oauth-creds CONSUMER_KEY
+                             CONSUMER_SECRET
+                             ACCESS_TOKEN
+                             ACCESS_TOKEN_SECRET))
+
 (defn get-tweets [handle]
-    "Return the tweets from the username 'handle'."
-    handle)
+  "Return the most recent tweets (up to 200) from the username 'handle'."
+  (statuses-user-timeline :oauth-creds creds :params {:screen-name handle :count 200}))
+
+(defn get-tweet-blob [handle]
+  "Returns a single string of all the most recent tweets for username 'handle'."
+  (clojure.string/join " " (map #(:text %) (:body (get-tweets handle)))))
---
+++ src/senior_project_profiles_server/watson.clj
@@ -0,0 +1,27 @@
+(ns senior-project-profiles-server.watson
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json]))
+
+(def API_URL "https://gateway.watsonplatform.net/personality-insights/api/v2/profile")
+(def USER "358d1379-52d4-4179-8ff9-b27c594b48bc")
+(def PASSWORD "hRs4gERTeSDa")
+
+(defn get-insights [text]
+    "Return the JSON object from Watson's personality insights API based on the given text."
+    (json/read-str 
+        (:body (client/post API_URL
+            {:basic-auth [USER PASSWORD]
+            :headers {"content-type" "text/plain"}
+            :body text
+        }))))
+
+(defn parse-insights [insights]
+    "Given the map response from Watson, return a map with the big 5 scores."
+    (let [personality (get (into {} (get (into {} (filter #(= (get % "id") "personality") (get (get insights "tree") "children"))) "children")) "children")
+          get-percentile (fn [trait jsonblob] (get (into {} (filter #(= (get % "id") trait) jsonblob)) "percentage"))]
+        {:Openness (get-percentile "Openness" personality)
+        :conscientiousness (get-percentile "Conscientiousness" personality)
+        :Extraversion (get-percentile "Extraversion" personality)
+        :Agreeableness (get-percentile "Agreeableness" personality)
+        :Neroticism (get-percentile "Neuroticism" personality)}))
+
